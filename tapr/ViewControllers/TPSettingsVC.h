1132449b738dffd0dbf39dc9adb56da77170d428
dregatos <david.regatos@gmail.com>
2014-06-18 17:48:53 +0200

    added SettingsVC;

---
+++ tapr/ViewControllers/TPSettingsVC.h
@@ -0,0 +1,13 @@
+//
+//  TPSettingsVC.h
+//  tapr
+//
+//  Created by David Regatos on 18/06/14.
+//  Copyright (c) 2014 ByteFly Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPSettingsVC : TPBaseVC
+
+@end
